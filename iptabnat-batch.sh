f0a14c4ddab3a0910c2111fdb373c5fca885fa70
zzfafa <zzfafa@qq.com>
2020-02-25 23:25:21 +0800

    Update iptabnat-batch.sh

--- iptabnat-batch.sh
+++ iptabnat-batch.sh
@@ -3,9 +3,14 @@ read -p "本地起始端口：" portup
 read -p "内网 NAT 地址：" ip
 read -p "远程转发端口：" port
 read -p "待转发的 ip 地址(以,号分隔)：" str
+read -p "清空现在所有转发的 IP (y/n 默认 n )：" delall
 OLD_IFS="$IFS"
 IFS="," 
 strs=($str)
+if [ $delall == "y" ]; then
+  iptables -t nat -F
+  echo "已清空现在所有转发的 IP"
+fi
 
 for(( i=0 ;i<${#strs[@]};i++))
 do
@@ -14,4 +19,8 @@ iptables -t nat -A PREROUTING -p udp -m udp --dport $[portup + i] -j DNAT --to-d
 iptables -t nat -A POSTROUTING -d ${strs[i]} -p tcp -m tcp --dport $port -j SNAT --to-source $ip
 iptables -t nat -A POSTROUTING -d ${strs[i]} -p udp -m udp --dport $port -j SNAT --to-source $ip
 done
+-A RH-Firewall-1-INPUT -m state --state NEW -m tcp -p tcp --dport &portup : $[portup + i] -j ACCEPT
+echo "成功添加 $i 个转发 IP"
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
 service iptables save
