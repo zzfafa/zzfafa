9504b1575790c29ca1c0d4481e1a8faacd2a4a17
zzfafa <zzfafa@qq.com>
2020-02-26 00:55:39 +0800

    Update iptabnat-batch.sh

--- iptabnat-batch.sh
+++ iptabnat-batch.sh
@@ -23,7 +23,7 @@ echo "添加端口 $[portup + i] 转发至 ${strs[i]}:$port 成功"
 done
 echo "成功添加 $i 个转发 IP"
 #iptables -I INPUT -p tcp --dport $[portup]:$[portup + i] -j ACCEPT
-echo "端口已开放 $[portup]:$[portup + i]"
+#echo "端口已开放 $[portup]:$[portup + i]"
 
 iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
