acd3f880f40f0b92ee330c544305f37d87efd3c9
zzfafa <zzfafa@qq.com>
2020-04-23 11:21:02 +0800

    Update iptabnatauto.sh

--- iptabnatauto.sh
+++ iptabnatauto.sh
@@ -20,7 +20,7 @@ iptables -t nat -A PREROUTING -p udp -m udp --dport $[portup + i] -j DNAT --to-d
 iptables -t nat -A POSTROUTING -d ${strs[i]} -p tcp -m tcp --dport $port -j SNAT --to-source $ip
 iptables -t nat -A POSTROUTING -d ${strs[i]} -p udp -m udp --dport $port -j SNAT --to-source $ip
 iptables -I INPUT -p tcp --dport $[portup + i] -j ACCEPT
-echo "添加端口 $[portup + i] 转发至 ${strs[i]}:$port 成功"
+#echo "添加端口 $[portup + i] 转发至 ${strs[i]}:$port 成功"
 done
 echo "成功添加 $i 个转发 IP"
 #iptables -I INPUT -p tcp --dport $[portup]:$[portup + i] -j ACCEPT
